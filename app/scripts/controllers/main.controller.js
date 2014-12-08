5b3dc2bcf070ed5cb528a90d9c2b81cceff3e50c
Benoit VALLON <benoitvallon@gmail.com>
2014-12-08 16:09:02 +0100

    Adds sample marks

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -1,3 +1,12 @@
 angular.module('markticle').controller('MainController', function($scope) {
-  $scope.marks = [];
+  $scope.marks = [
+  {
+    title: 'Smashing magazine',
+    url: 'http://www.smashingmagazine.com/'
+  },
+  {
+    title: 'Markticle',
+    url: 'https://markticle.com'
+  }
+  ];
 });
